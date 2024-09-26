3fc91b3a78e5e5e1082f0ba3893e929e857c94e7
oliveira-helio <helio.oliveira.paulo@gmail.com>
2024-09-26 19:18:35 -0300

    update

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,18 @@
-// alert("teste");
+
+
+const menuHamburger = document.querySelector('.menu-hamburger');
+
+menuHamburger.addEventListener('click', () => {
+    toggleMenu();
+});
+
+function toggleMenu() {
+    const nav = document.querySelector('.nav-responsive');
+    menuHamburger.classList.toggle('change');
+
+    if(menuHamburger.classList.contains('change')){
+        nav.style.display = 'block';
+    } else {
+        nav.style.display = 'none';
+    }
+}
